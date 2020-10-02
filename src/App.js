f05d24983124ed40e53dc3e73e65615cec334f7b
dcanavitas <cj@navitasconsulting.net>
2020-10-02 02:51:48 +0100

    .

--- src/App.js
+++ src/App.js
@@ -12,12 +12,11 @@ function App() {
     <>
       <Router>
         <NavBar/>
-        <Home/>
         <Switch>
-          <Route path='/' exact component={Home}/>
-          <Route path='/products' exact component={Products}/>
-          <Route path='/services' exact component={Services}/>
-          <Route path='/sign-up' exact component={SignUp}/>
+          <Route path={process.env.PUBLIC_URL + '/'} exact component={Home}/>
+          <Route path={process.env.PUBLIC_URL + '/products'} exact component={Products}/>
+          <Route path={process.env.PUBLIC_URL + '/services'} exact component={Services}/>
+          <Route path={process.env.PUBLIC_URL + '/sign-up'} exact component={SignUp}/>
         </Switch>
       </Router>
     </>
