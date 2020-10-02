9610170f75083a01931dd69e55aee1f7e9117945
dcanavitas <cj@navitasconsulting.net>
2020-10-02 06:28:42 +0100

    .

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,7 +4,7 @@ import CardItem from './CardItem'
 function Cards() {
     return (
         <div className="cards">
-            <h1>Check out these epic destinations</h1>
+            <h1>Check out these artworks</h1>
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
