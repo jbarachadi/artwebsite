183ef9a055c973b793b0e5286f0c534a4a59632f
dcanavitas <cj@navitasconsulting.net>
2020-10-02 02:40:05 +0100

    Updates

---
+++ precache-manifest.f8ea8b33bbbb85162be6ae5fa40e96e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3492d68433b13e9163631d777c9a4e0c",
+    "url": "/artwebsite/index.html"
+  },
+  {
+    "revision": "1bc69984f4eced02d760",
+    "url": "/artwebsite/static/css/main.3f5e4b2f.chunk.css"
+  },
+  {
+    "revision": "c8e6b061271f2e6c1641",
+    "url": "/artwebsite/static/js/2.a703b9bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/artwebsite/static/js/2.a703b9bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc69984f4eced02d760",
+    "url": "/artwebsite/static/js/main.3c6eb1ce.chunk.js"
+  },
+  {
+    "revision": "2c3109ad3461b496a248",
+    "url": "/artwebsite/static/js/runtime-main.193478aa.js"
+  }
+]);
