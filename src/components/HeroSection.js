9a4a89b4fa676c200883b9588c0d2f08fefcdca2
dcanavitas <cj@navitasconsulting.net>
2020-10-02 15:41:26 +0100

    .

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,4 @@
 import React from 'react'
-/* import {Button} from './Button' */
 import '../css/App.css'
 import '../css/HeroSection.css'
 
@@ -7,13 +6,6 @@ function HeroSection() {
     return (
         <div className="hero-container">
             <h1>Welcome<span className="colored-char">.</span></h1>
-            {/* <video src="/videos/video-1.mp4" autoPlay loop muted/>
-            <p>What are you waiting for?</p>
-            <div className="hero-btns">
-                <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">
-                    Get Started
-                </Button>
-            </div>*/}
         </div>
     )
 }
